8b0cb32720a9042c845c8bcc5b07ac42ea1f8d83
Ma Xinze <2460798168@qq/com>
2021-03-07 19:59:50 +0800

    :art: DFS

--- leetcode-java/src/cn/com/codingce/permuteunique/Solution.java
+++ leetcode-java/src/cn/com/codingce/permuteunique/Solution.java
@@ -62,7 +62,7 @@ public class Solution {
     }
 
     public static void main(String[] args) {
-        int[] nums = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+        int[] nums = {1, 2, 3};
         Solution solution = new Solution();
         List<List<Integer>> lists = solution.permute(nums);
         System.out.println(lists);
