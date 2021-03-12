89bc7ba2fabf3e003e4d2dbeca3ea796c377bccc
Ma Xinze <2460798168@qq.com>
2021-03-12 11:18:56 +0800

    :fire: min, max, sum

---
+++ leetcode-java/src/cn/com/codingce/lq/base/MaxMinSum.java
@@ -0,0 +1,27 @@
+package cn.com.codingce.lq.base;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class MaxMinSum {
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int[] array = new int[n];
+		for (int i = 0; i < array.length; i++) {
+			array[i] = sc.nextInt();
+		}
+
+		Arrays.sort(array);
+		System.out.println(array[0]);
+		System.out.println(array[n - 1]);
+		int sum = 0;
+		for (int i : array) {
+			sum += i;
+		}
+		System.out.println(sum);
+
+	}
+
+}
