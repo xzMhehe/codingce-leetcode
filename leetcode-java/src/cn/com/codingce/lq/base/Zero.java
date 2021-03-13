5df95e13adf460fd8a61fe9f54dedac6ca9f86c5
Ma Xinze <2460798168@qq.com>
2021-03-13 14:45:22 +0800

    :fire: 圆的面积

---
+++ leetcode-java/src/cn/com/codingce/lq/base/Zero.java
@@ -0,0 +1,16 @@
+package cn.com.codingce.lq.base;
+
+import java.util.Scanner;
+
+public class Zero {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double input = sc.nextDouble();
+        double area = Math.pow(input, 2) * Math.PI;
+//        area = area * 10000000 + 0.5;
+        System.out.printf("%.7f", area);
+        sc.close();
+
+
+    }
+}
