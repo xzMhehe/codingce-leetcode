a4a53fd40661e52bead6db2e577844ba519b0e51
Ma Xinze <2460798168@qq.com>
2021-03-13 08:10:18 +0800

    :fire: 字母图案

---
+++ leetcode-java/src/cn/com/codingce/lq/base/LetterPic.java
@@ -0,0 +1,22 @@
+package cn.com.codingce.lq.base;
+
+import java.util.Scanner;
+
+public class LetterPic {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+
+		int[][] array = new int[a][b];
+		for (int i = 0, n = a; i < n; i++) {
+			for (int j = 0; j < b; j++) {
+				System.out.print((char) ('A' + Math.abs((i - j))));
+			}
+			System.out.println();
+
+		}
+		
+		sc.close();
+	}
+}
