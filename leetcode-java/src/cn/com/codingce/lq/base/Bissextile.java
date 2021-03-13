052b31706f66e51bd74d41b05bf48f161ddc8b80
Ma Xinze <2460798168@qq.com>
2021-03-13 08:11:22 +0800

    :fire: 闰年

---
+++ leetcode-java/src/cn/com/codingce/lq/base/Bissextile.java
@@ -0,0 +1,19 @@
+package cn.com.codingce.lq.base;
+
+import java.util.Scanner;
+
+public class Bissextile {
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		int input = sc.nextInt();
+		if (input % 4 == 0 && input % 100 != 0 || input % 400 == 0) {
+			System.out.println("yes");
+		} else {
+			System.out.println("no");
+		}
+		sc.close();
+
+	}
+
+}
