d7c37239d6a1fc914f76f2404cce698149d8a91d
Ma Xinze <2460798168@qq.com>
2021-07-16 10:29:43 +0800

    A 快排思想

--- leetcode-java/src/cn/com/codingce/myarray/tet/Solution.java
+++ leetcode-java/src/cn/com/codingce/myarray/tet/Solution.java
@@ -11,7 +11,7 @@ package cn.com.codingce.myarray.tet;
 public class Solution {
     public static void main(String[] args) {
         Solution solution = new Solution();
-        solution.moveZeroes(new int[]{1, 2, 3, 0, 1});
+        solution.moveZeroes2(new int[]{1, 2, 3, 0, 1});
 
     }
 
@@ -34,6 +34,15 @@ public class Solution {
         }
     }
 
+    /**
+     * 这里参考了快速排序的思想，快速排序首先要确定一个待分割的元素做中间点x，然后把所有小于等于x的元素放到x的左边，大于x的元素放到其右边。
+     * 这里我们可以用0当做这个中间点，把不等于0(注意题目没说不能有负数)的放到中间点的左边，等于0的放到其右边。
+     * 这的中间点就是0本身，所以实现起来比快速排序简单很多，我们使用两个指针i和j，只要nums[i]!=0，我们就交换nums[i]和nums[j]
+     * 请对照动态图来理解：
+     *
+     *
+     * @param nums
+     */
     public void moveZeroes2(int[] nums) {
         if (nums == null) {
             return;
