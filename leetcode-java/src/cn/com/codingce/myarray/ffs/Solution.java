1aa3d2770716bf9472ac1ef209e5fa22462661b1
Ma Xinze <2460798168@qq.com>
2021-07-20 08:38:15 +0800

    A 557. 反转字符串中的单词 III

--- leetcode-java/src/cn/com/codingce/myarray/ffs/Solution.java
+++ leetcode-java/src/cn/com/codingce/myarray/ffs/Solution.java
@@ -2,12 +2,13 @@ package cn.com.codingce.myarray.ffs;
 
 /**
  * 557. 反转字符串中的单词 III
+ *
  * @author williamma
  */
 public class Solution {
     public static void main(String[] args) {
 
-        System.out.println(new Solution().reverseWords("Let's take LeetCode contest"));
+        System.out.println(new Solution().reverseWords2("Let's take LeetCode contest"));
 
     }
 
@@ -26,5 +27,40 @@ public class Solution {
         return sb.toString();
 
     }
+
+    /**
+     * 此时的原地解法的意义就失去了，因为又开辟出一块空间
+     *
+     * @param s
+     * @return
+     */
+    public String reverseWords2(String s) {
+        StringBuffer sb = new StringBuffer(s);
+        int len = sb.length();
+        int i = 0;
+        while (i < len) {
+            int start = i;
+            while (i < len && sb.charAt(i) != ' ') {
+                i++;
+            }
+
+            int left = start, right = i - 1;
+            while (left < right) {
+                char tmp = sb.charAt(left);
+                sb.setCharAt(left, sb.charAt(right));
+                sb.setCharAt(right, tmp);
+
+                left++;
+                right--;
+            }
+
+            while (i < len && sb.charAt(i) == ' ') {
+                i++;
+            }
+        }
+
+        return sb.toString();
+    }
+
 
 }
