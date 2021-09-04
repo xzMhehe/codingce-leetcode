15f5938c2c723b9ada5abdca81059723b2043f3a
Ma Xinze <2460798168@qq.com>
2021-09-05 07:35:38 +0800

    F 测试

--- leetcode-go/src/lesson01_hello_word/lession1.go
+++ leetcode-go/src/lesson01_hello_word/lession1.go
@@ -48,8 +48,16 @@ func add(a, b int) int {
 	return a + b
 }
 
+func add2(a, b int) int {
+	return a + b
+}
+
 //牛逼的地方来了，支持多个返回值
 func reverse(a, b, c string) (string, string, string) {
+	return c, b, a
+}
+
+func reverse3(a, b, c string) (string, string, string) {
 	return c, b, a
 }
 
@@ -58,11 +66,23 @@ func reverse1(a, b, c string) (x string, y string, z string) {
 	return
 }
 
+func reverse2(a, b, c string) (x string, y string, z string) {
+	x, y, z = c, b, a
+	return
+
+}
+
 //又一个牛逼的写法，命名返回值，没有参数的 return 语句返回已命名的返回值。
 //直接返回语句应当仅用在下短函数中。否则会影响代码的可读性。
 //如果函数中不给y赋值，则y便是默认值（int就是0）
 func split(sum int) (x, y int) {
 	x = sum * 4 / 9
+	y = sum - x
+	return
+}
+
+func split2(sum int) (x, y int) {
+	x = sum * 5 / 9
 	y = sum - x
 	return
 }
