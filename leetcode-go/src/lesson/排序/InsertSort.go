95bf1cc649a829d9b667313d96c8f6a9f659ef9c
Ma Xinze <2460798168@qq.com>
2021-09-07 10:04:08 +0800

    A 插入、选择、冒泡

---
+++ leetcode-go/src/lesson/排序/InsertSort.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("插入排序：", insertSort([]int{1, 2, 5, 3, 9, 4, 10}))
+}
+
+/*
+插入排序的代码实现虽然没有冒泡排序和选择排序那么简单粗暴，但它的原理应该是最容易理解的了，
+因为只要打过扑克牌的人都应该能够秒懂。插入排序是一种最简单直观的排序算法，它的工作原理是通过构建有序序列，
+对于未排序数据，在已排序序列中从后向前扫描，找到相应位置并插入。
+插入排序和冒泡排序一样，也有一种优化算法，叫做拆半插入。
+
+*/
+func insertSort(array []int) []int {
+	for i := range array {
+		perIndex := i - 1
+		current := array[i]
+		for perIndex > 0 && array[perIndex] > current {
+			array[perIndex+1] = array[perIndex]
+			perIndex -= 1
+		}
+		array[perIndex+1] = current
+	}
+
+	return array
+}
---
+++ leetcode-go/src/lesson/排序/SelectionSort.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	fmt.Println("选择排序：", selectSort([]int{1, 2, 5, 3, 9, 4, 10}))
+}
+
+/*
+选择排序是一种简单直观的排序算法，无论什么数据进去都是 O(n²) 的时间复杂度。所以用到它的时候，数据规模越小越好。
+唯一的好处可能就是不占用额外的内存空间了吧。
+*/
+func selectSort(array []int) []int {
+	length := len(array)
+	for i := 0; i < length; i++ {
+		min := i
+		for j := i + 1; j < length; j++ {
+			if array[min] > array[j] {
+				min = j
+			}
+		}
+
+		array[i], array[min] = array[min], array[i]
+	}
+
+	return array
+
+}
