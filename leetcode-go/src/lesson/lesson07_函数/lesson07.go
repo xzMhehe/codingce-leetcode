06ce272609a476e1aac175a80e4b86c5870df24f
Ma Xinze <2460798168@qq.com>
2021-09-10 21:32:14 +0800

    F 函数

--- leetcode-go/src/lesson/lesson07_函数/lesson07.go
+++ leetcode-go/src/lesson/lesson07_函数/lesson07.go
@@ -11,6 +11,14 @@ func main() {
 
 	sum := addAll(3, 4, 5, 6, 7)
 	fmt.Println(sum)
+
+	foo1("111", 7)
+
+	ma, mb := foo2("1", 2)
+	fmt.Println("ma = ", ma, " mb = ", mb)
+
+	mr1, mr2 := foo3("1", 2)
+	fmt.Println("mr1 = ", mr1, " mr2 = ", mr2)
 }
 
 // kelvinToCelsius converts °K to °C
@@ -26,4 +34,32 @@ func addAll(a int, numbers ...int) int {
 		sum += v
 	}
 	return sum
+}
+
+func foo1(a string, b int) int {
+	fmt.Println("a = ", a)
+	fmt.Println("b = ", b)
+	c := 100
+	return c
+}
+
+// 返回多个返回值匿名的
+func foo2(a string, b int) (int, int) {
+	fmt.Println("a = ", a)
+	fmt.Println("b = ", b)
+	c := 100
+	return c, 77
+}
+
+// 返回多个返回值有形参的
+func foo3(a string, b int) (r1 int, r2 int) {
+	r1 = 100
+	r2 = 200
+	return
+}
+
+func foo4(a string, b int) (r1, r2 int) {
+	r1 = 100
+	r2 = 200
+	return
 }
