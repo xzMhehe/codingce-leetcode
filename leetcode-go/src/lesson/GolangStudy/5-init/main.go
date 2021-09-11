64414a66acc415dfa13d8895f859b1fe87acf570
Ma Xinze <2460798168@qq.com>
2021-09-11 09:37:28 +0800

    A 切片

---
+++ leetcode-go/src/lesson/GolangStudy/5-init/main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"lesson/GolangStudy/5-init/lib1"
+	"lesson/GolangStudy/5-init/lib2"
+	mylib2 "lesson/GolangStudy/5-init/lib2"
+)
+
+func main() {
+	lib1.Lib1Test()
+	mylib2.Lib2Test()
+	lib2.Lib2Test()
+}
