28766fd0cd3a4a3313f9ef98b8d2cbfca226c939
Ma Xinze <2460798168@qq.com>
2021-09-14 20:31:01 +0800

    A post请求

--- leetcode-go/src/mybeego/controllers/hello.go
+++ leetcode-go/src/mybeego/controllers/hello.go
@@ -1,6 +1,8 @@
 package controllers
 
 import (
+	"fmt"
+
 	"github.com/astaxie/beego"
 )
 
@@ -12,4 +14,13 @@ func (c *HelloController) Get() {
 	c.Data["Website"] = "codingce.com.cn"
 	c.Data["Email"] = "codingce@gmail.com"
 	c.TplName = "hello.tpl"
+}
+func (c *HelloController) Post() {
+	// 接受post请求
+	c.Ctx.WriteString("post 的提交方式 codingce codingce")
+	// 接受请求变量
+	name := c.GetString("name")
+	pass := c.GetString("pass")
+	fmt.Println("你的账号：" + name + "你的密码：" + pass)
+	c.Ctx.WriteString("你的账号：" + name + "你的密码：" + pass)
 }
