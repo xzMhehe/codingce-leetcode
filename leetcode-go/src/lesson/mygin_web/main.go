160371cd878df46c3d23b0078b99e333aa41cf68
Ma Xinze <2460798168@qq.com>
2021-09-18 09:28:12 +0800

    F 146

--- leetcode-go/src/lesson/mygin_web/main.go
+++ leetcode-go/src/lesson/mygin_web/main.go
@@ -1,41 +1,26 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
-	// 实例化一个gin
 	r := gin.Default()
-	r.GET("/ping", sayHello)
-
-	r.GET("/book", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "GET",
+	r.LoadHTMLGlob("templates/**/*")
+	//r.LoadHTMLFiles("templates/posts/index.html", "templates/users/index.html")
+	r.GET("/posts/index", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "posts/index.html", gin.H{
+			"title": "posts/index",
 		})
 	})
 
-	r.POST("/book", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "POST",
+	r.GET("users/index", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "users/index.html", gin.H{
+			"title": "users/index",
 		})
 	})
 
-	r.PUT("/book", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "PUT",
-		})
-	})
-
-	r.DELETE("/book", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "DELETE",
-		})
-	})
-
-	r.Run(":8091") // 监听并在 0.0.0.0:8080 上启动服务
-}
-
-func sayHello(c *gin.Context) {
-	c.JSON(200, gin.H{
-		"message": "pong",
-	})
+	r.Run(":8080")
 }
