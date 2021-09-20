d1c136c99e5d74d8c3a5d94bdfc832cbfc2885b6
Ma Xinze <2460798168@qq.com>
2021-09-20 09:13:34 +0800

    A 198. House Robber

---
+++ leetcode-go/src/动态规划/打家劫舍198/Solution.go
@@ -0,0 +1,49 @@
+package main
+
+import "fmt"
+
+func main() {
+	nums := []int{2, 1}
+	fmt.Printf("rob(nums): %v\n", rob(nums))
+}
+
+func rob(nums []int) int {
+	n := len(nums)
+	if n == 0 {
+		return 0
+	}
+	if n == 1 {
+		return nums[0]
+	}
+	dp := make([]int, n)
+	dp[0] = nums[0]
+	dp[1] = max(nums[0], nums[1])
+	for i := 2; i < n; i++ {
+		dp[i] = max(dp[i-2]+nums[i], dp[i-1])
+	}
+	return dp[n-1]
+
+}
+
+func rob2(nums []int) int {
+	n := len(nums)
+	if n == 0 {
+		return 0
+	}
+	if n == 1 {
+		return nums[0]
+	}
+	first := nums[0]
+	second := max(nums[0], nums[1])
+	for i := 2; i < n; i++ {
+		first, second = second, max(first+nums[i], second)
+	}
+	return second
+}
+
+func max(x, y int) int {
+	if x > y {
+		return x
+	}
+	return y
+}
