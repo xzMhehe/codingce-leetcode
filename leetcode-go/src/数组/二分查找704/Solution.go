964053a4ee1c41366f147e39e9a25090df4667e6
Ma Xinze <2460798168@qq.com>
2021-09-21 17:19:06 +0800

    A 第一个错误的版本278

--- leetcode-go/src/数组/二分查找704/Solution.go
+++ leetcode-go/src/数组/二分查找704/Solution.go
@@ -4,6 +4,7 @@ import "fmt"
 
 func main() {
 	fmt.Printf("search(): %v\n", search([]int{5}, 5))
+	fmt.Printf("search2(): %v\n", search2([]int{5}, 5))
 }
 
 func search(nums []int, target int) int {
@@ -14,6 +15,21 @@ func search(nums []int, target int) int {
 			return mid
 		} else if nums[mid] > target {
 			right = mid - 1
+		} else if nums[mid] < target {
+			left = mid + 1
+		}
+	}
+	return -1
+}
+
+func search2(nums []int, target int) int {
+	left, right, mid := 0, len(nums)-1, 0
+	for left <= right {
+		mid = left + (right-left)>>1
+		if nums[mid] > target {
+			right = mid - 1
+		} else if nums[mid] == target {
+			return mid
 		} else if nums[mid] < target {
 			left = mid + 1
 		}
---
+++ leetcode-go/src/数组/第一个错误的版本278/Solution.go
@@ -0,0 +1,26 @@
+package main
+
+func main() {
+
+}
+
+/**
+ * Forward declaration of isBadVersion API.
+ * @param   version   your guess about first bad version
+ * @return 	 	      true if current version is bad
+ *			          false if current version is good
+ * func isBadVersion(version int) bool;
+ */
+
+func firstBadVersion(n int) int {
+	left, right := 0, n
+	for left <= right {
+		mid := left + (right-left)>>1
+		if isBadVersion(mid) {
+			right = mid - 1
+		} else {
+			left = mid + 1
+		}
+	}
+	return left
+}
