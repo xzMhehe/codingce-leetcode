c0d5a5885c0d7bcb2a6480c1003e3996f506a441
Ma Xinze <2460798168@qq.com>
2021-09-21 17:03:13 +0800

    A go二分

---
+++ leetcode-go/src/二分查找704/Solution.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Printf("search(): %v\n", search([]int{5}, 5))
+}
+
+func search(nums []int, target int) int {
+	left, right, mid := 0, len(nums)-1, 0
+	for left <= right {
+		mid = left + (right-left)>>1
+		if nums[mid] == target {
+			return mid
+		} else if nums[mid] > target {
+			right = mid - 1
+		} else if nums[mid] < target {
+			left = mid + 1
+		}
+	}
+	return -1
+}
