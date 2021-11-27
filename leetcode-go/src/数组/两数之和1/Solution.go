9ea26211c6879cce5a77d669c3c76f83c761f326
Ma Xinze <2460798168@qq.com>
2021-11-27 09:04:19 +0800

    两数之和

---
+++ leetcode-go/src/数组/两数之和1/Solution.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main() {
+	nums := []int{1, 2, 5, 7, 9, 10}
+	target := 7
+	fmt.Printf("两数之和: %+v, target: %d\n", twoSum(nums, target), target)
+}
+
+func twoSum(nums []int, target int) []int {
+	m := make(map[int]int)
+	for i := 0; i < len(nums); i++ {
+		another := target - nums[i]
+		if _, ok := m[another]; ok {
+			return []int{m[another], i}
+		}
+		m[nums[i]] = i
+	}
+
+	return nil
+}
