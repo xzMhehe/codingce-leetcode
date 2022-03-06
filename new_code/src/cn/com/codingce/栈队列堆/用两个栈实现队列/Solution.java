f3364dd67cad7815aeea59de383905cb9c428d7a
Ma Xinze <2460798168@qq.com>
2022-03-06 13:45:26 +0800

    笔记

--- new_code/src/cn/com/codingce/栈队列堆/用两个栈实现队列/Solution.java
+++ new_code/src/cn/com/codingce/栈队列堆/用两个栈实现队列/Solution.java
@@ -6,8 +6,6 @@ import java.util.Stack;
 
 public class Solution {
     public static void main(String[] args) {
-
-
     }
     //in
     Stack<Integer> stack1 = new Stack<Integer>();
@@ -31,6 +29,13 @@ public class Solution {
 }
 class CQueue {
     //力扣
+    /**
+     * 用两个栈实现一个队列。队列的声明如下，请实现它的两个函数 appendTail 和 deleteHead ，
+     * 分别完成在队列尾部插入整数和在队列头部删除整数的功能。(若队列中没有元素，deleteHead 操作返回 -1 )
+     *
+     * 来源：力扣（LeetCode）
+     * 链接：https://leetcode-cn.com/problems/yong-liang-ge-zhan-shi-xian-dui-lie-lcof
+     */
     Deque<Integer> stack3;
     Deque<Integer> stack4;
 
