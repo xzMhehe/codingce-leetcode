363518aa821707dc21ab725d6f3ebf329aea5adc
Ma Xinze <2460798168@qq.com>
2022-03-09 18:49:38 +0800

    A

---
+++ new_code/src/cn/com/codingce/链表/ListNode.java
@@ -0,0 +1,10 @@
+package cn.com.codingce.链表;
+
+public class ListNode {
+    int val;
+    ListNode next = null;
+
+    ListNode(int val) {
+        this.val = val;
+    }
+}
---
+++ new_code/src/cn/com/codingce/链表/链表中环的入口结点/Solution.java
@@ -0,0 +1,25 @@
+package cn.com.codingce.链表.链表中环的入口结点;
+
+import cn.com.codingce.链表.ListNode;
+
+/**
+ * 链表中环的入口结点
+ *
+ * 给一个长度为n链表，若其中包含环，请找出该链表的环的入口结点，否则，返回null。
+ *
+ * @author inke219223m
+ */
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    public ListNode EntryNodeOfLoop(ListNode pHead) {
+        //边界
+        if (pHead == null) {
+            return null;
+        }
+
+
+    }
+}
