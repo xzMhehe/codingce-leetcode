7fef28f27d940e329d0aef4baa830a715c9b8602
Ma Xinze <2460798168@qq.com>
2022-03-10 23:20:59 +0800

    readme

---
+++ new_code/src/cn/com/codingce/链表/RandomListNode.java
@@ -0,0 +1,11 @@
+package cn.com.codingce.链表;
+
+public class RandomListNode {
+    public int label;
+    public RandomListNode next = null;
+    public RandomListNode random = null;
+
+    RandomListNode(int label) {
+        this.label = label;
+    }
+}
