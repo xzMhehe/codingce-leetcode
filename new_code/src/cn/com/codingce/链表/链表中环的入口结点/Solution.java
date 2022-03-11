5c113548c81837e11d984ec31923a4c26bcc02b0
Ma Xinze <2460798168@qq.com>
2022-03-11 09:46:14 +0800

    F

--- new_code/src/cn/com/codingce/链表/链表中环的入口结点/Solution.java
+++ new_code/src/cn/com/codingce/链表/链表中环的入口结点/Solution.java
@@ -4,27 +4,36 @@ import cn.com.codingce.链表.ListNode;
 
 /**
  * 链表中环的入口结点
- *
+ * <p>
  * 给一个长度为n链表，若其中包含环，请找出该链表的环的入口结点，否则，返回null。
  *
  * @author inke219223m
  */
 public class Solution {
     public static void main(String[] args) {
-
+        System.out.println(new Solution().EntryNodeOfLoop(new ListNode(1, null)));
     }
 
     public ListNode EntryNodeOfLoop(ListNode pHead) {
-        //边界
-        if (pHead == null) {
+        if (pHead == null || pHead.next == null) {
             return null;
         }
-        ListNode low = null;
-        ListNode fast = null;
-        while (pHead != null) {
-            pHead = pHead.next;
-        }
 
-        return fast;
+        ListNode fast = pHead;
+        ListNode slow = pHead;
+
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+            if (fast == slow) {
+                ListNode slow2 = pHead;
+                while (slow2 != slow) {
+                    slow2 = slow2.next;
+                    slow = slow.next;
+                }
+                return slow2;
+            }
+        }
+        return null;
     }
 }
