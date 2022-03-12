b5a29243691ee1b32e6527310486a16fb20a4bd5
Ma Xinze <2460798168@qq.com>
2022-03-12 08:20:58 +0800

    跳台阶

---
+++ new_code/src/cn/com/codingce/动态规划/跳台阶/Solution.java
@@ -0,0 +1,24 @@
+package cn.com.codingce.动态规划.跳台阶;
+
+/**
+ * 一只青蛙一次可以跳上 1 级台阶，也可以跳上 2 级。求该青蛙跳上一个 n 级的台阶总共有多少种跳法。
+ *
+ * @author maxinze
+ */
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println(new Solution().jumpFloor(5));
+    }
+
+    public int jumpFloor(int target) {
+        if (target <= 2)
+            return target;
+        int pre1 = 1, pre2 = 2, result = 0;
+        for (int i = 3; i < target; i++) {
+            result = pre1 + pre2;
+            pre1 = pre2;
+            pre2 = result;
+        }
+        return result;
+    }
+}
