3572bbef9be38e89b317c5e9ba7400f6d4a42bbf
Ma Xinze <2460798168@qq.com>
2022-03-12 08:28:58 +0800

    斐波那契数列

---
+++ new_code/src/cn/com/codingce/动态规划/斐波那契数列/Solution.java
@@ -0,0 +1,30 @@
+package cn.com.codingce.动态规划.斐波那契数列;
+
+/**
+ * 求斐波那契数列的第 n 项，n <= 39。
+ */
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println(new Solution().Fibonacci(4));
+    }
+
+    public int Fibonacci(int n) {
+        if (n <= 2) {
+            return 1;
+        }
+        return Fibonacci(n - 1) + Fibonacci(n - 2);
+    }
+
+    public int Fibonacci2(int n) {
+        if (n <= 2) {
+            return 1;
+        }
+        int pre1 = 1, pre2 = 2, result = 3;
+        for (int i = 3; i < n; i++) {
+            result = pre1 + pre2;
+            pre1 = pre2;
+            pre2 = result;
+        }
+        return result;
+    }
+}
