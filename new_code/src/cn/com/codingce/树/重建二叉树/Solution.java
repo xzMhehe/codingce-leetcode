f13f38eb33bb8ff4ebb80c926ec8050dbe91f97f
Ma Xinze <2460798168@qq.com>
2022-03-13 20:32:47 +0800

    重建二叉树

--- new_code/src/cn/com/codingce/树/重建二叉树/Solution.java
+++ new_code/src/cn/com/codingce/树/重建二叉树/Solution.java
@@ -7,19 +7,35 @@ import java.util.Map;
 
 public class Solution {
     public static void main(String[] args) {
-
+        System.out.println(new Solution().reConstructBinaryTree(
+                new int[]{1, 2, 4, 7, 3, 5, 6, 8},
+                new int[]{4, 7, 2, 1, 5, 3, 8, 6}));
 
     }
 
     // 缓存中序遍历数组每个值对应的索引
     private Map<Integer, Integer> indexForInOrders = new HashMap<>();
 
+
+    /**
+     * @param pre 先根遍历
+     * @param in 中根遍历
+     * @return
+     */
     public TreeNode reConstructBinaryTree(int[] pre, int[] in) {
         for (int i = 0; i < in.length; i++)
+            // 4 0 ｜ 7 1 ｜ 2 3 | ...
             indexForInOrders.put(in[i], i);
         return reConstructBinaryTree(pre, 0, pre.length - 1, 0);
     }
 
+    /**
+     * @param pre 先根遍历
+     * @param preL
+     * @param preR
+     * @param inL
+     * @return
+     */
     private TreeNode reConstructBinaryTree(int[] pre, int preL, int preR, int inL) {
         if (preL > preR) return null;
         TreeNode root = new TreeNode(pre[preL]);
