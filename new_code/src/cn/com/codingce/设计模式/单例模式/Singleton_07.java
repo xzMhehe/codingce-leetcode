1f8ba6b89afba15566c3adbd820e3a3bc975d914
Ma Xinze <2460798168@qq.com>
2022-03-26 22:45:09 +0800

    枚举单例（线程安全）

---
+++ new_code/src/cn/com/codingce/设计模式/单例模式/Singleton_07.java
@@ -0,0 +1,23 @@
+package cn.com.codingce.设计模式.单例模式;
+
+/**
+ * 枚举单例（线程安全）
+ *
+ * 这种方式在功能上与共有域方法相近，但更加简洁，无偿提供串行化机制，绝对防止对此实例化。
+ * 但是在继承场景下不可用
+ *
+ * public void test() {
+ * 	Singleton_07.INSTANCE.test();
+ * }
+ *
+ * @author inke219223m
+ */
+public enum Singleton_07 {
+
+    INSTANCE;
+
+    public void test() {
+        System.out.println("hi~");
+    }
+}
+
