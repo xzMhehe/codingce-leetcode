f3518eb9c6d999078951b71da74b509c34476a9d
Ma Xinze <2460798168@qq.com>
2022-12-31 11:01:42 +0800

    F 组合模式

--- new_code/src/cn/com/codingce/设计模式/组合模式/CompositePatternDemo.java
+++ new_code/src/cn/com/codingce/设计模式/组合模式/CompositePatternDemo.java
@@ -1,5 +1,15 @@
 package cn.com.codingce.设计模式.组合模式;
 
+/**
+ * 何时使用： 1、您想表示对象的部分-整体层次结构（树形结构）；
+ * 希望用户忽略组合对象与单个对象的不同，用户将统一地使用组合结构中的所有对象。
+ * 优点：高层模块调用简单；节点自由增加。
+ * 缺点：在使用组合模式时，其叶子和树枝的声明都是实现类，而不是接口，违反了依赖倒置原则。
+ * 依赖倒置原则（Dependence Inversion Principle）是程序要依赖于抽象接口，不要依赖于具体实现。
+ * 简单的说就是要求对抽象进行编程，不要对实现进行编程，这样就降低了客户与实现模块间的耦合。
+ *
+ * @author mxz
+ */
 public class CompositePatternDemo {
 
     public static void main(String[] args) {
