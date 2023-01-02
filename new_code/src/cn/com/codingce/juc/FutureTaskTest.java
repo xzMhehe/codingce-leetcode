079a47e0bb247e038f9aee2abb2d7ef4ee00116c
Ma Xinze <2460798168@qq.com>
2023-01-02 09:48:25 +0800

    Callable

---
+++ new_code/src/cn/com/codingce/juc/FutureTaskTest.java
@@ -0,0 +1,23 @@
+package cn.com.codingce.juc;
+
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.FutureTask;
+
+public class FutureTaskTest {
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        MyCallable mc = new MyCallable();
+        FutureTask<Integer> ft = new FutureTask<>(mc);
+        Thread thread = new Thread(ft);
+        thread.start();
+        System.out.println(ft.get());
+        System.out.println(Integer.MAX_VALUE);
+    }
+}
+
+class MyCallable implements Callable<Integer> {
+    @Override
+    public Integer call() {
+        return 123;
+    }
+}
