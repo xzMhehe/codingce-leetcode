0c6913204f002585dc0d837a1e4a3797e2c301cc
Ma Xinze <2460798168@qq.com>
2023-01-02 18:35:03 +0800

    A CyclicBarrierExample

---
+++ new_code/src/cn/com/codingce/juc/aqs/CyclicBarrierExample.java
@@ -0,0 +1,27 @@
+package cn.com.codingce.juc.aqs;
+
+import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.CyclicBarrier;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class CyclicBarrierExample {
+    public static void main(String[] args) {
+        final int totalThread = 10;
+        CyclicBarrier cyclicBarrier = new CyclicBarrier(totalThread);
+        ExecutorService executorService = Executors.newCachedThreadPool();
+        for (int i = 0; i < totalThread; i++) {
+            int finalI = i;
+            executorService.execute(() -> {
+                System.out.print("before.." + finalI + " ");
+                try {
+                    cyclicBarrier.await();
+                } catch (InterruptedException | BrokenBarrierException e) {
+                    e.printStackTrace();
+                }
+                System.out.print("after.." + finalI + " ");
+            });
+        }
+        executorService.shutdown();
+    }
+}
