dc72a7e0909d7c149eeddbe9fcb02caa64f2bc75
Ma Xinze <2460798168@qq.com>
2023-01-14 10:32:04 +0800

    F 判断该字符串是否有效

---
+++ new_code/src/cn/com/codingce/数组与矩阵/检查符号是否成对出现/Solution.java
@@ -0,0 +1,40 @@
+package cn.com.codingce.数组与矩阵.检查符号是否成对出现;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Stack;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        System.out.println(new Solution().isValid("(){}[]"));
+    }
+
+    /**
+     * 给定一个只包括 '('，')'，'{'，'}'，'['，']' 的字符串，判断该字符串是否有效。有效字符串需满足：
+     * 左括号必须用相同类型的右括号闭合。
+     * 左括号必须以正确的顺序闭合。比如 "()"、"()[]{}"、"{[]}" 都是有效字符串，而 "(]" 、"([)]" 则不是。
+     *
+     * @param str
+     * @return
+     */
+    public boolean isValid(String str) {
+        Map<Character, Character> map = new HashMap<>();
+        map.put(')', '(');
+        map.put('}', '{');
+        map.put(']', '[');
+        Stack<Character> stack = new Stack<>();
+        char[] chars = str.toCharArray();
+        for (Character c : chars) {
+            if (map.containsKey(c)) {
+                char topElement = stack.empty() ? '#' : stack.pop();
+                if (topElement != map.get(c)) {
+                    return false;
+                }
+            } else {
+                stack.push(c);
+            }
+        }
+        return stack.isEmpty();
+    }
+}
