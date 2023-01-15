f8f48b7e4287f50c8afebbf2f3675c161c390c50
Ma Xinze <2460798168@qq.com>
2023-01-15 13:58:07 +0800

    F 倒数第k个节点

--- new_code/src/cn/com/codingce/链表/链表中倒数最后k个结点/Solution.java
+++ new_code/src/cn/com/codingce/链表/链表中倒数最后k个结点/Solution.java
@@ -1,6 +1,13 @@
 package cn.com.codingce.链表.链表中倒数最后k个结点;
 
+import cn.com.codingce.链表.ListNode;
+
 public class Solution {
+    /**
+     * 输入一个链表，输出该链表中倒数第k个结点
+     *
+     * @param args
+     */
     public static void main(String[] args) {
         System.out.println(new Solution().FindKthToTail(new ListNode(2, new ListNode(3, new ListNode(5, new ListNode(2, null)))), 3));
     }
@@ -33,18 +40,31 @@ public class Solution {
         return slow;
     }
 
-}
+    /**
+     * 时间复杂度：O(2*n)
+     * 空间复杂度：O(1)
+     *
+     * @param pHead
+     * @param k
+     * @return
+     */
+    public ListNode FindKthToTail2(ListNode pHead, int k) {
+        if (pHead == null || k < 0) return null;
+        // 所有节点个数
+        int n = 0;
+        ListNode cur = pHead;
+        while (cur != null) {
+            cur = cur.next;
+            ++n;
+        }
+        if (n < k) return null;
+        // 正向的位置
+        n -= k;
+        while (n-- != 0) {
+            pHead = pHead.next;
+        }
 
-class ListNode {
-    int val;
-    ListNode next = null;
-
-    public ListNode(int val) {
-        this.val = val;
+        return pHead;
     }
 
-    public ListNode(int val, ListNode next) {
-        this.val = val;
-        this.next = next;
-    }
 }
