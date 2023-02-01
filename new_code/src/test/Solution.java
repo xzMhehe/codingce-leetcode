b0d992933538b776d4dd72cc9b6569653f5fa373
Ma Xinze <2460798168@qq.com>
2023-02-01 13:06:30 +0800

    F

--- new_code/src/test/Solution.java
+++ new_code/src/test/Solution.java
@@ -1,7 +1,10 @@
 package test;
 
+import java.util.ArrayDeque;
+
 public class Solution {
     public static void main(String[] args) {
         System.out.println(5&7);
+        new ArrayDeque<>();
     }
 }
