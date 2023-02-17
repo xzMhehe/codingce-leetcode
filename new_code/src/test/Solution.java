16c0f76ae2be2746fdf0ecbc63f1b9b0d675ef94
Ma Xinze <2460798168@qq.com>
2023-02-17 13:47:57 +0800

    F 逆波兰表达式

--- new_code/src/test/Solution.java
+++ new_code/src/test/Solution.java
@@ -1,10 +1,46 @@
 package test;
 
-import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.LinkedList;
 
 public class Solution {
+
     public static void main(String[] args) {
-        System.out.println(5&7);
-        new ArrayDeque<>();
+        System.out.println(new Solution().evalRPN(new String[]{"2", "1", "+", "3", "*"}));
+    }
+
+    public int evalRPN(String[] tokens) {
+        Deque<Integer> stack = new LinkedList<Integer>();
+        int n = tokens.length;
+        for (int i = 0; i < n; i++) {
+            String token = tokens[i];
+            if (isNumber(token)) {
+                stack.push(Integer.parseInt(token));
+            } else {
+                int num2 = stack.pop();
+                int num1 = stack.pop();
+                switch (token) {
+                    case "+":
+                        stack.push(num1 + num2);
+                        break;
+                    case "-":
+                        stack.push(num1 - num2);
+                        break;
+                    case "*":
+                        stack.push(num1 * num2);
+                        break;
+                    case "/":
+                        stack.push(num1 / num2);
+                        break;
+                    default:
+                }
+            }
+        }
+        return stack.pop();
+    }
+
+    public boolean isNumber(String token) {
+        return !("+".equals(token) || "-".equals(token) || "*".equals(token) || "/".equals(token));
     }
 }
+
