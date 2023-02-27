d4397eef902eab6ea784316a7cc8299098abbf97
Ma Xinze <2460798168@qq.com>
2023-02-27 10:01:10 +0800

    A 正则替换

---
+++ new_code/src/test/Test.java
@@ -0,0 +1,23 @@
+package test;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.regex.PatternSyntaxException;
+
+public class Test {
+    public static void main(String[] args) {
+        String test = "428.9a#@~";
+        System.out.println(StringFilter(test));
+    }
+
+    // Filter UnNumbe rCharacters RegexExpress
+    static String regEx = "[^-?\\d+.?\\d+]|([-+])";
+
+    public static String StringFilter(String str) throws PatternSyntaxException {
+        Pattern pattern = Pattern.compile(regEx);
+        Matcher matcher = pattern.matcher(str);
+        System.out.println(matcher.matches());
+        return matcher.replaceAll("").trim();
+    }
+
+}
