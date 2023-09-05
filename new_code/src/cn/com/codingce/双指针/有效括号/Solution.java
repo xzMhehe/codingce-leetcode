7f7ca51adbaf9facd6428671f356af3f13874b5a
Ma Xinze <2460798168@qq.com>
2023-09-05 17:20:43 +0800

    A 有效括号

---
+++ new_code/src/cn/com/codingce/双指针/有效括号/Solution.java
@@ -0,0 +1,40 @@
+package cn.com.codingce.双指针.有效括号;
+
+import java.util.Stack;
+
+/**
+ * 给定一个只包括 '('，')'，'{'，'}'，'['，']' 的字符串 s ，判断字符串是否有效。
+ * 有效字符串需满足：
+ * 左括号必须用相同类型的右括号闭合。
+ * 左括号必须以正确的顺序闭合。
+ * 每个右括号都有一个对应的相同类型的左括号。
+ */
+public class Solution {
+
+    public static void main(String[] args) {
+        System.out.println(new Solution().isValid("{}()[]"));
+    }
+
+    public boolean isValid(String str) {
+        Stack<Character> left = new Stack<>();
+        for (char c : str.toCharArray()) {
+            if (c == '(' || c == '{' || c == '[')
+                left.push(c);
+            else // 字符 c 是右括号
+                if (!left.isEmpty() && leftOf(c) == left.peek())
+                    left.pop();
+                else
+                    // 和最近的左括号不匹配
+                    return false;
+        }
+        // 是否所有的左括号都被匹配了
+        return left.isEmpty();
+    }
+
+    char leftOf(char c) {
+        if (c == '}') return '{';
+        if (c == ')') return '(';
+        return '[';
+    }
+
+}
